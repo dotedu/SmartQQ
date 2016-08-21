0d519ab74a5dec18b727fde46d9bd610805fca9a
魚跃 <pisces.hj@gmail.com>
2016-08-22 00:51:21 +0800

    Create

---
+++ QQlogin/LoginForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace QQlogin
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.QrPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.QrPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // QrPicture
+            // 
+            this.QrPicture.Location = new System.Drawing.Point(60, 21);
+            this.QrPicture.Name = "QrPicture";
+            this.QrPicture.Size = new System.Drawing.Size(165, 165);
+            this.QrPicture.TabIndex = 0;
+            this.QrPicture.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.QrPicture);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)(this.QrPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox QrPicture;
+    }
+}
